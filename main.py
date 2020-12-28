67821e37c9f4c3e0bed9075ca9697e168ab58de2
Dubinin Dmitrii <dmitriid204@ya.ru>
2020-12-28 20:57:12 +0500

    Небольшой шаблон

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+import os
+import sys
+import math
+from copy import deepcopy
+import random
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def normal_exit():
+    """Нормальный выход из игры (с вызовом титров)"""
+
+
+def end_screen():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
