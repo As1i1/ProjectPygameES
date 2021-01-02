2ea05a5c9651dc1add2601276504c46b8b22d15c
Bloodmain <krytoipohtalon@mail.ru>
2021-01-02 21:24:05 +0500

    Более-менне адекватная физика (Однако плохо работает с другими спрайтами)

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ class Asphalt(pygame.sprite.Sprite):
         self.image = load_image(r'Background/Constructions/asphalt.png')
         self.rect = self.image.get_rect().move(
             TILE_WIDTH * pos_x, TILE_HEIGHT * pos_y)
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 class Hero(pygame.sprite.Sprite):
@@ -57,76 +58,65 @@ class Hero(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = load_image(r'Sprites\Semen\Walk (1) — копия.png')
         self.rect = self.image.get_rect().move(pos_x * TILE_WIDTH, pos_y * TILE_HEIGHT - self.image.get_width() // 2)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.w, self.rect.h = self.mask.get_bounding_rects()[0].w + 2, self.mask.get_bounding_rects()[0].h + 2
+
         self.lower_bound = 200
         self.upper_bound = 600
         self.vx = 50
-        self.vy = 0
+        self.vy = 50
         self.dx = 0
+
+        self.jump_timer = 0
 
     def update(self, *args, **kwargs):
         keys = pygame.key.get_pressed()
 
-        # Проверка на пересечение
-        bounds1 = pygame.sprite.spritecollide(self, bound_group, False)
+        cant_jump = False   # Что бы нельзя было прыгать в воздухе
 
-        # Отбор спрайтов которые пересекаются по маске
-        bounds = []
-        for sprite in bounds1:
-            if sprite is None:
-                continue
-            coord = pygame.sprite.collide_mask(sprite, self)
-            if coord:
-                print(*coord)
-                bounds.append((sprite, *coord))
-        print()
-
-        # Проверка куда можно пойти
-        GoLeft, GoRight, GoUp, GoDown = True, True, True, True
-        x2, y2, w2, h2 = self.rect.x, self.rect.y, self.rect.w, self.rect.h
-        for sprite, x, y in bounds:
-            x1, y1, w1, h1 = sprite.rect.x, sprite.rect.y, sprite.rect.w, sprite.rect.h
-            if y1 + h1 >= y2 > y1 and (min(x1, x2) <= max(x1, x2) <= min(x1 + w1, x2 + w2) or
-                                       max(x1, x2) <= min(x1 + w1, x2 + w2) <= max(x1 + w1, x2 + w2)) and \
-                    (x != 0 and x != TILE_WIDTH - 1):
-                GoUp = False
-            if y2 + h2 >= y1 > y2 and (min(x1, x2) <= max(x1, x2) <= min(x1 + w1, x2 + w2) or
-                                       max(x1, x2) <= min(x1 + w1, x2 + w2) <= max(x1 + w1, x2 + w2)) and \
-                    (x != 0 and x != TILE_WIDTH - 1):
-                GoDown = False
-            if x2 + w2 >= x1 > x2 and (min(y1, y2) <= max(y1, y2) <= min(y1 + h1, y2 + h2) or
-                                       max(y1, y2) <= min(y1 + h1, y2 + h2) <= max(y1 + h1, y2 + h2)) and \
-                    (y != 0 and y != TILE_HEIGHT - 1):
-                GoRight = False
-            if x1 + w1 >= x2 > x1 and (min(y1, y2) <= max(y1, y2) <= min(y1 + h1, y2 + h2) or
-                                       max(y1, y2) <= min(y1 + h1, y2 + h2) <= max(y1 + h1, y2 + h2)) and \
-                    (y != 0 and y != TILE_HEIGHT - 1):
-                GoLeft = False
-
-        # Передвижение
-        if keys[pygame.K_RIGHT]:
-            if GoRight:
-                self.rect.x += math.ceil(self.vx / FPS)
-        if keys[pygame.K_LEFT]:
-            if GoLeft:
-                self.rect.x -= math.ceil(self.vx / FPS)
-        if keys[pygame.K_UP]:
-            if not GoDown:
-                self.vy = 150
-
-        if self.vy > 0:
-            if not GoUp:
-                self.vy = 0
-            else:
+        # Если идёт "анимация" прыжка
+        if self.jump_timer != 0:
+            # Если мы ни во что не упираемся сверху
+            if not pygame.sprite.spritecollideany(self, bound_group):
                 self.rect.y -= math.ceil(self.vy / FPS)
-                self.vy -= 1
+                self.jump_timer -= 1
+                cant_jump = True
+            else:
+                self.jump_timer = 0
 
-        if GoDown and self.vy <= 0:
-            self.vy = -10
-            self.rect.y += math.ceil(abs(self.vy) / FPS)
-        elif not GoDown and self.vy == -10:
-            self.vy = 0
+        # Если персонаж не пересекается с асфальтом, значит он падает
+        elif not self.collide_mask_asphalt():
+            self.rect.y += math.ceil(self.vy / FPS)
+            cant_jump = True
+
+        # Перемещаем персонажа
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += math.ceil(self.vx / FPS)
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= math.ceil(self.vx / FPS)
+        if keys[pygame.K_UP] and not cant_jump:
+            self.jump_timer = 150
+
+        # Проверим на пересечение
+        for collide in pygame.sprite.spritecollide(self, bound_group, False):
+            # Если мы мы ударились во что-то сверху, то ничего не делаем
+            if collide.rect.y + collide.rect.h <= self.rect.y + 1:
+                continue
+            # Иначе, "вытолкнем" персонажа влево или вправо, в зависимости от его положения
+            elif self.rect.x > collide.rect.x:
+                self.rect.x = collide.rect.x + collide.rect.w
+                return
+            else:
+                self.rect.x = collide.rect.x - self.rect.w
+                return
 
         self.check_bounds()
+
+    def collide_mask_asphalt(self):
+        for sprite in bound_group:
+            if pygame.sprite.collide_mask(self, sprite):
+                return True
+        return False
 
     def check_bounds(self):
         self.dx = max(min(self.upper_bound - self.rect.x, 0),
@@ -183,7 +173,7 @@ def move_background(bg_first, bg_second):
     bg_second.rect.x %= img_width
 
 
-def play_game():  # TODO Сделать игру:D ага *****; за буквами следи;
+def play_game():            # TODO Сделать игру:D ага *****; за буквами следи;
     # TODO Пацагы меня не хватит мне срочно нужен супермаркет. Мое сердечко так страдает. Мне нужен супермаркет. Вита, Виталиночка.
     """Запуск игры (игрового цикла)"""
 
@@ -265,8 +255,7 @@ if __name__ == '__main__':
         time_delta = clock.tick(FPS) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                terminate()  # Временно
-            #                confirm_exit()
+                confirm_exit()
             if event.type == pygame.USEREVENT:
                 if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
                     running = False
@@ -281,9 +270,9 @@ if __name__ == '__main__':
 
                         play_game()
                     if event.ui_element == load_game_btn:
-                        """Загружаем сохранения"""  # TODO сделать сохранения
+                        """Загружаем сохранения"""          # TODO сделать сохранения
                     if event.ui_element == show_achievements_btn:
-                        """Показываем достижения"""  # TODO сделать ачивки
+                        """Показываем достижения"""         # TODO сделать ачивки
                     if event.ui_element == exit_btn:
                         confirm_exit()
 
